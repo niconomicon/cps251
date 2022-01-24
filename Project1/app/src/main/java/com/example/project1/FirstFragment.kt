87bf12ece1cdeaf7d26ad8b22e8362e9683388a6
niconomicon <nwilliam4@wccnet.edu>
2022-01-24 16:40:14 -0500

    update

--- Project1/app/src/main/java/com/example/project1/FirstFragment.kt
+++ Project1/app/src/main/java/com/example/project1/FirstFragment.kt
@@ -13,19 +13,18 @@ import com.example.project1.databinding.FragmentFirstBinding
  */
 class FirstFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
-
+private var _binding: FragmentFirstBinding? = null
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
-        return binding.root
+      _binding = FragmentFirstBinding.inflate(inflater, container, false)
+      return binding.root
 
     }
 
@@ -37,7 +36,7 @@ class FirstFragment : Fragment() {
         }
     }
 
-    override fun onDestroyView() {
+override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
--- Project1/app/src/main/java/com/example/project1/SecondFragment.kt
+++ Project1/app/src/main/java/com/example/project1/SecondFragment.kt
@@ -13,19 +13,18 @@ import com.example.project1.databinding.FragmentSecondBinding
  */
 class SecondFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
-
+private var _binding: FragmentSecondBinding? = null
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
-        return binding.root
+      _binding = FragmentSecondBinding.inflate(inflater, container, false)
+      return binding.root
 
     }
 
@@ -36,8 +35,7 @@ class SecondFragment : Fragment() {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
     }
-
-    override fun onDestroyView() {
+override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
