c93a2be9ad1ed3be72ff8371bd6c8f3560e354cb
niconomicon <nwilliam4@wccnet.edu>
2022-02-28 13:52:22 -0500

    made function to save names

--- AddNameSaveData1/app/src/main/java/com/example/addnamesavedata1/ui/main/MainViewModel.kt
+++ AddNameSaveData1/app/src/main/java/com/example/addnamesavedata1/ui/main/MainViewModel.kt
@@ -4,4 +4,15 @@ import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+
+
+    fun saveName(){
+        var userText = textView.text.toString()
+        textView4.setText(userText)
+        //do something with text
+        var namesList = ""
+        var newList = namesList + userText
+        println(newList)
+
+    }
 }
