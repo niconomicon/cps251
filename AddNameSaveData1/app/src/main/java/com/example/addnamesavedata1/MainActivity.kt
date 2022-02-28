0b4119e49f7471a556595aee62e9823430bab9b3
niconomicon <nwilliam4@wccnet.edu>
2022-02-28 17:40:35 -0500

    modified project to use view bindings

--- AddNameSaveData1/app/src/main/java/com/example/addnamesavedata1/MainActivity.kt
+++ AddNameSaveData1/app/src/main/java/com/example/addnamesavedata1/MainActivity.kt
@@ -3,12 +3,18 @@ package com.example.addnamesavedata1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.addnamesavedata1.ui.main.MainFragment
+import com.example.addnamesavedata1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
