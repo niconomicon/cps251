c50d9f5bc22a9c0a8562ce926d0d1da7207d2221
niconomicon <nwilliam4@wccnet.edu>
2022-03-17 17:16:48 -0400

    created project structure

---
+++ LifeCycleAwareProject/app/src/main/java/com/example/lifecycleawareproject/ui/main/MainFragment.kt
@@ -0,0 +1,69 @@
+package com.example.lifecycleawareproject.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.lifecycleawareproject.R
+import com.example.lifecycleawareproject.DemoObserver
+import com.example.lifecycleawareproject.DemoOwner
+
+/*
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}*/
+
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+
+    private lateinit var demoOwner: DemoOwner
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        lifecycle.addObserver(DemoObserver())
+
+        //THESE METHODS ARE FOR THE CUSTOM CLASS.
+        demoOwner = DemoOwner()
+        demoOwner.startOwner()
+        demoOwner.stopOwner()
+
+    }
+
+}
