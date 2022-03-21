f5eb7b0bbe33b91cd9bf2340123b9cc778c70522
niconomicon <nwilliam4@wccnet.edu>
2022-03-21 10:38:04 -0400

    added data binding

--- LifeCycleAwareProject/app/src/main/java/com/example/lifecycleawareproject/ui/main/MainFragment.kt
+++ LifeCycleAwareProject/app/src/main/java/com/example/lifecycleawareproject/ui/main/MainFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import com.example.lifecycleawareproject.R
 import com.example.lifecycleawareproject.DemoObserver
 import com.example.lifecycleawareproject.DemoOwner
+import com.example.lifecycleawareproject.databinding.MainFragmentBinding
 
 /*
 class MainFragment : Fragment() {
@@ -41,13 +42,23 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding!!
+
     private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+//        return inflater.inflate(R.layout.main_fragment, container, false)
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 
@@ -55,14 +66,30 @@ class MainFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+    ///NEEDS WORK
+        val resultObserver = Observer<Float> { result ->
+            binding.resultText.text = result.toString()
+
+             viewModel.getResult().observe(viewLifecycleOwner, resultObserver)
+        }
+
+        binding.convertButton.setOnClickListener {
+            if (binding.dollarText.text.isNotEmpty()) {
+                viewModel.setAmount(binding.dollarText.text.toString())
+            } else {
+                binding.resultText.text = "No Value"
+            }
+        }
+//NEEDS WORK
         lifecycle.addObserver(DemoObserver())
 
         //THESE METHODS ARE FOR THE CUSTOM CLASS.
         demoOwner = DemoOwner()
         demoOwner.startOwner()
         demoOwner.stopOwner()
+
 
     }
 
