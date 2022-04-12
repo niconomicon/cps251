33375b18b03cc91b37c59e8c3f0cc65ebd1273e5
niconomicon <nwilliam4@wccnet.edu>
2022-04-11 21:44:56 -0400

    edited comments

--- CardDemoWithIntents/app/src/main/java/com/ebookfrenzy/carddemo/RecyclerAdapter.kt
+++ CardDemoWithIntents/app/src/main/java/com/ebookfrenzy/carddemo/RecyclerAdapter.kt
@@ -19,7 +19,6 @@ import kotlin.random.Random
 
 class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
-    // created new Data class instance
     private lateinit var data: Data
     private lateinit var mainActivity: MainActivity
 
@@ -43,13 +42,12 @@ class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
             itemView.setOnClickListener { v: View ->
                 var position: Int = getAdapterPosition()
-                //added calls to title, details, and img data to make sure it's pulling the right info
+                //added calls to title, details, and img data to make sure it's pulling the right info for Intents
                 Snackbar.make(v, "Click detected on item $position, " + itemTitle.text + ", " + itemDetail.text  + ", Image #" + randomImgNum,
                     Snackbar.LENGTH_LONG).setAction("Action", null).show()
 
-                // put intents info here (i think???)
 
-                // Tried putting Intents info here, but it isn't an activity so it doesn't work
+                // Tried putting Intents info here, but it isn't an activity so it didn't work
                 /*val i = Intent(this, MainActivity2::class.java)
 
                     i.putExtra("myTitle", itemTitle.text)
@@ -68,35 +66,15 @@ class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
                      // val myString = binding.editText1.text.toString()
                  }*/
 
-                // Tried calling method from Main Activity, but it breaks in sendText method on Intent creation line
+                // Tried calling method from Main Activity, but it breaks in the sendText method on the Intent creation line
                 //mainActivity.sendText(itemView)
-
-
-                //I don't think I need this, kept for reference
-                //binding = ActivityMainBinding.bind(itemView)
 
             }
         }
     }
 
-    //previous location of data, kept for reference
-    /*
-    private val titles = arrayOf("Chapter One",
-        "Chapter Two", "Chapter Three", "Chapter Four",
-        "Chapter Five", "Chapter Six", "Chapter Seven",
-        "Chapter Eight")
-    private val details = arrayOf("Item one details", "Item two details",
-        "Item three details", "Item four details",
-        "Item five details", "Item six details",
-        "Item seven details", "Item eight details")
-    private val images = intArrayOf(R.drawable.android_image_1,
-        R.drawable.android_image_2, R.drawable.android_image_3,
-        R.drawable.android_image_4, R.drawable.android_image_5,
-        R.drawable.android_image_6, R.drawable.android_image_7,
-        R.drawable.android_image_8)
-        */
 
-    //mutable lists to hold randomized data for saved instance, so it can be displayed again when redrawn
+
     private var randomTitles = mutableListOf<Int>();
     private var randomDetails = mutableListOf<Int>();
     private var randomImages = mutableListOf<Int>();
@@ -110,73 +88,30 @@ class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
-        // initialized instance of data class
+
         data = Data()
 
-        // choosing random ints for array, between 0-7
         val randomTitle : Int = (0..7).random()
         val randomDetail : Int = (0..7).random()
         val randomImage : Int = (0..7).random()
 
-        //add random image number to view holder variable so it can be called later
+        //add random image number to view holder variable so it can be passed using Intents
         viewHolder.randomImgNum = randomImage
 
-        // randomizing data called from Data class
         viewHolder.itemTitle.text = data.getTitle(randomTitle)
         viewHolder.itemDetail.text = data.getDetail(randomDetail)
         viewHolder.itemImage.setImageResource(data.getImage(randomImage))
 
-        //adding data to mutable lists in this class
         randomTitles.add(randomTitle)
         randomDetails.add(randomDetail)
         randomImages.add(randomImage)
 
-        //adding data to mutable lists in Data class - doesn't work, new data instance is created each time so it only adds 1
-        /*
-        data.setRandomTitles(randomTitle)
-        data.setRandomDetails(randomDetail)
-        data.setRandomImages(randomImage)
-        */
-
-        //randomized data called from this class, kept for reference
-        /*
-        viewHolder.itemTitle.text = titles[randomTitle]
-        println("Random Titles: "+ randomTitles)
-        viewHolder.itemDetail.text = details[randomDetail]
-        println("Random Details: "+ randomDetails)
-        viewHolder.itemImage.setImageResource(images[randomImage])
-        println("Random Images: "+ randomImages)
-        */
-
-        //un-randomized data called from this class, kept for reference
-        /*
-        viewHolder.itemTitle.text = titles[i]
-        viewHolder.itemDetail.text = details[i]
-        viewHolder.itemImage.setImageResource(images[i])
-        */
-
-        // testing variables, kept just for reference
-        //var i: Int = (0..7).random()
-        //println(data.getTitle((0..7).random()))
-        //println("Random Titles: " + randomTitles)
-        //println("Random Title #: " + randomTitle)
-        //println("Random Detail #: "+ randomDetail)
-        //println("Random Image #: " + randomImage)
-        //println("Random Titles: "+ data.getRandomTitles())
-        //println("Random Details: "+ data.getRandomDetails())
-        //println("Random Images: "+ data.getRandomImages())
-
     }
 
     override fun getItemCount(): Int {
-        //call to array in original location, kept for reference
-        //return titles.size
 
-        //initializing new instance of Data class and calling to array in Data class
         data = Data()
         return data.getTitles().size
-
-
 
     }
 }
