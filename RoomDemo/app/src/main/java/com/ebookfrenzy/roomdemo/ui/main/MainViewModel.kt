843bbbf0b2c159ad672c9d9aa8e9a897188a2874
niconomicon <nwilliam4@wccnet.edu>
2022-04-19 14:54:05 -0400

    added classes and functions

--- RoomDemo/app/src/main/java/com/ebookfrenzy/roomdemo/ui/main/MainViewModel.kt
+++ RoomDemo/app/src/main/java/com/ebookfrenzy/roomdemo/ui/main/MainViewModel.kt
@@ -1,7 +1,39 @@
 package com.ebookfrenzy.roomdemo.ui.main
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.ebookfrenzy.roomdemo.Product
+import com.ebookfrenzy.roomdemo.ProductRepository
+//import androidx.lifecycle.ViewModel
 
-class MainViewModel : ViewModel() {
+
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
     // TODO: Implement the ViewModel
+    private val repository: ProductRepository = ProductRepository(application)
+    private val allProducts: LiveData<List<Product>>?
+    private val searchResults: MutableLiveData<List<Product>>
+
+    init {
+        allProducts = repository.allProducts
+        searchResults = repository.searchResults
+    }
+
+    fun insertProduct(product: Product) {
+        repository.insertProduct(product)
+    }
+    fun findProduct(name: String) {
+        repository.findProduct(name)
+    }
+    fun deleteProduct(name: String) {
+        repository.deleteProduct(name)
+    }
+    fun getSearchResults(): MutableLiveData<List<Product>> {
+        return searchResults
+    }
+    fun getAllProducts(): LiveData<List<Product>>? {
+        return allProducts
+    }
 }
